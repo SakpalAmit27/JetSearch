fef88a3b40318cd930e732c78e85c48b7dc4966f
Sakpal Amit <amitsakpal744@gmail.com>
2024-12-16 14:10:44 +0530

    created the search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,3 +1,5 @@
+import { Search } from "lucide-react";
+import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
 const SearchBar = () => {
@@ -6,6 +8,8 @@ const SearchBar = () => {
         <div className="relative w-full h-14 flex flex-col bg-white">
             <div className="relative h-14 z-10 rounded-md">
                 <Input className="absolute inset-0 h-full"/>
+                
+                <Button className="absolute right-0 inset-y-0 h-full rounded-l-none"><Search className="h-6 w-6"/></Button>
             </div>
         </div>
     )
